deb4eac20dca31e2934faa492aa0a12bdf4a6bbc
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-30 20:59:35 +0100

    Added snack

--- js/script-01.js
+++ js/script-01.js
@@ -1,38 +1,35 @@
-// write a function that generate random different numbers, with selected amount of numbers
-var arrayLength = parseInt(prompt('Insert the array length between 1 and 100'));
-console.log(arrayLength);
-var arrayNumber = [];
-arrayNumber = arrayGenerator(arrayNumber, arrayLength);
-console.log(arrayNumber);
+$(document).ready(function() {
 
-// function
-// random number generator between min max
-function getRandomNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  var bikes = [
+    {
+      'name' : 'fast',
+      'weight' : 5
+    },
+    {
+      'name' : 'slow',
+      'weight' : 8
+    },
+    {
+      'name' : 'quick',
+      'weight' : 3
+    },
+    {
+      'name' : 'mid',
+      'weight' : 7
+    }
+  ]
 
-// generate array with random numbers
-function arrayGenerator(array, length) {
-  var i = 0;
-  var number = 0;
-  while (i < length) {
-    number = getRandomNumber(1, 100);
-    if (!array.includes(number)) {
-      array[i] = number;
-      i++;
+  console.log(bikes);
+
+  // print the bike that has less weight than the others
+  var lessWeight = bikes[0];
+  for (var i = 0; i < bikes.length; i++) {
+    var bike = bikes[i];
+    if (bike['weight'] < lessWeight.weight) {
+      lessWeight = bike;
     }
   }
-  return array;
-}
 
-// scan array
-// function arrayScan(array, item) {
-//   for (var i = 0; i < array.length; i++) {
-//     if (array[i] === item) {
-//       return true;
-//     }
-//   }
-//   return false;
-// }
+  console.log(lessWeight);
+
+});
